523079e62ba301a71604eb380a638008bd73679c
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-05-10 15:22:50 +0530

    finished app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from "react";
 import "./App.css";
-
 function App() {
-  return <div>
-    
-  </div>;
+  return (
+    <div className="con">
+      <form className="contact-form" method="POST">
+        <label>Name</label>
+        <input placeholder="Enter name..."></input>
+        <label>Email</label>
+        <input placeholder="Enter email..."></input>
+        <label>Enter Message</label>
+        <input placeholder="Enter message..."></input>
+        <button type="submit">Send Message</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
